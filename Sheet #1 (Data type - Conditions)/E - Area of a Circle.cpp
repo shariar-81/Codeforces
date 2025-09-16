82641995360a7bbf6d2e7c8e32e46e8f511d31ab
Md. Shariar Bin Alam <abir3935@gmail.com>
2025-09-17 03:15:36 +0600

    Create E - Area of a Circle.cpp

---
+++ Sheet #1 (Data type - Conditions)/E - Area of a Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    double R;
+    cin >> R;
+ 
+    const double PI = 3.141592653;
+    double area = PI * R * R;
+ 
+    cout << setprecision(9) << area << endl;
+ 
+    return 0;
+}
